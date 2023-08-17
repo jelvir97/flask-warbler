44ffb3885a46b653f9f69f84fc025027390ee5c7
Juan Elvir <jelvir97@gmail.com>
2023-08-17 10:46:18 -0600

    logout route

--- app.py
+++ app.py
@@ -114,7 +114,9 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+    flash("See you next time warbler!","success")
+    return redirect('/')
 
 
 ##############################################################################
