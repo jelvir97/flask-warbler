ec501b592106f05bba313aade487e207d2c3fdea
Juan Elvir <jelvir97@gmail.com>
2023-08-24 09:05:14 -0600

    test user signup

---
+++ test_user_views.py
@@ -0,0 +1,75 @@
+"""User View tests."""
+
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Message, User
+from sqlalchemy.exc import NoResultFound
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+# Now we can import app
+
+from app import app, CURR_USER_KEY
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+
+        self.client = app.test_client()
+
+        self.testuser1 = User.signup(username="testuser1",
+                                    email="test1@test.com",
+                                    password="testuser1",
+                                    image_url=None)
+        
+        self.testuser2 = User.signup(username="testuser2",
+                                    email="test2@test.com",
+                                    password="testuser2",
+                                    image_url=None)
+
+        db.session.commit()
+
+    def test_user_signup(self):
+        """Test User Signup"""
+
+        with self.client as c:
+            
+            resp = c.post("/signup", data={"username": "testuser3",
+                                           "password":"testuser3",
+                                           "email":"test3@test.com",
+                                           "image_url":None})
+
+            # Make sure it redirects
+            self.assertEqual(resp.status_code, 302)
+
+            users = User.query.all()
+            self.assertEqual(len(users),3)
+            self.assertEqual(users[2].username,"testuser3")
